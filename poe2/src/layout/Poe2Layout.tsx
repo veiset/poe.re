cf0260005c1ccd5f9e9d064c6acb94f82c045a1d
dogan.ay <65234588+DayTF@users.noreply.github.com>
2026-08-27 16:50:34 +0200

    feat(poe2): league selector (#488)

--- poe2/src/layout/Poe2Layout.tsx
+++ poe2/src/layout/Poe2Layout.tsx
@@ -5,6 +5,7 @@ import Poe2PageLinks from "./Poe2PageLinks";
 import {selectedProfile} from "../localStorage";
 import CoffeeBanner from "@shared/components/banner/CoffeeBanner";
 import {useRefreshFromInitialLoad, useRefreshOnFocus} from "@shared/core/RefreshOnFocus";
+import {Poe2LeagueProvider} from "./Poe2LeagueContext";
 import "../poe2.css";
 
 export const Poe2Layout = () => {
@@ -15,23 +16,25 @@ export const Poe2Layout = () => {
 
   return (
     <Poe2ProfileContext.Provider value={{currentProfile, setCurrentProfile}}>
-      <CoffeeBanner/>
-      <div className="content-height-wrapper">
-        <div className="content-container">
-          <div className="content-links">
-            <Poe2PageLinks/>
-          </div>
-          <div className="content-main">
-            <div className="content-left-gfx"/>
-            <div className="content-main-area">
-              <div className="page-content" key={`poe2-${currentProfile}`}>
-                <Outlet/>
-              </div>
+      <Poe2LeagueProvider>
+        <CoffeeBanner/>
+        <div className="content-height-wrapper">
+          <div className="content-container">
+            <div className="content-links">
+              <Poe2PageLinks/>
             </div>
-            <div className="content-right-gfx"/>
+            <div className="content-main">
+              <div className="content-left-gfx"/>
+              <div className="content-main-area">
+                <div className="page-content" key={`poe2-${currentProfile}`}>
+                  <Outlet/>
+                </div>
+              </div>
+              <div className="content-right-gfx"/>
+            </div>
           </div>
         </div>
-      </div>
+      </Poe2LeagueProvider>
     </Poe2ProfileContext.Provider>
   );
 };
